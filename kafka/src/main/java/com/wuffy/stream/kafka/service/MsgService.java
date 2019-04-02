622e9d6b30d730c9b64ed75895fca898d02b16e4
wuguanqing <army6540@163.com>
2019-04-02 16:19:49 +0800

    kafak 代码模板

---
+++ kafka/src/main/java/com/wuffy/stream/kafka/service/MsgService.java
@@ -0,0 +1,13 @@
+package com.wuffy.stream.kafka.service;
+
+/**
+ * @author wuffy
+ * @version 1.0
+ * @date 2019-04-02 15:59
+ * @Description TODO
+ */
+public class MsgService {
+  public static void main(String[] args) {
+      System.out.print("");
+  }
+}
